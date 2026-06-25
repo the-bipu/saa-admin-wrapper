352cd63045d6fdb516236eb7028b8f62f59b0d95
The Bipu <127449591+the-bipu@users.noreply.github.com>
2026-06-25 15:22:37 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
         source={{
-          uri: "https://www.slietalumni.in/",
+          uri: "https://www.admin.slietalumni.in/",
         }}
         style={{ flex: 1 }}
       />
